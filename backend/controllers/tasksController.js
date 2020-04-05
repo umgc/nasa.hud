aa69e7f5ed7571e975e90eb9b3b4cd5677d6fa4e
Jonathan Johnson <jjohnson100481@gmail.com>
2020-04-05 19:22:14 -0400

    added task headers and MaestroStep key to group steps

--- backend/controllers/tasksController.js
+++ backend/controllers/tasksController.js
@@ -5,15 +5,15 @@ var taskDir = './tasks/';
 var taskCounter = 0;
 
 
-// Function to return the parsed steps for each task in the specified procedure file, given a role
+// Function to get tasks from procedure file for role selected by actor
 exports.get_tasks = function(req, res) {
     var response = [];
-    var actorKey; //added 3/27/20
-    var actorName; //added 3/27/20
+    var actorKey; 
+    var actorName; 
    
     try {
         const file = yaml.safeLoad(fs.readFileSync(procDir+req.params.filename, "utf8"));
-        for(const keys of file.columns){ //added 3/27/20
+        for(const keys of file.columns){ 
             if(keys.display === req.params.role)
                 actorKey = keys.key;
         }
@@ -23,17 +23,11 @@ exports.get_tasks = function(req, res) {
             var keys = Object.keys(role);
             for(var key of keys)
             {
-                if(role[key] === actorKey) //added 3/27/20
+                if(role[key] === actorKey) 
                 {   
-                    
                     var out = get_steps(obj.file, key);
-                   
-                   
-                   // console.log(out);
-                   // console.log('--------------------------');
                     if(out.length != undefined)
                     {
-                        //var out = get_steps(obj.file, key);
                         for(var step of out){
                             if(typeof step === "object")
                                 response.push(...step);
@@ -46,19 +40,12 @@ exports.get_tasks = function(req, res) {
                 }
             }
         }
-       
-        //console.log(JSON.stringify(response));
-
-        var r = decompose(response);
-        for(const i of response){
-            console.log(JSON.stringify(i));
-        }
-
+       var r = decompose(response);
         
         
     }
     catch(e){
-        // Handle file not found exception
+        
         if (e.code === 'ENOENT') {
             res.status(404).send({"error": "The selected file does not exist. Please try again."});
         }
@@ -72,7 +59,7 @@ exports.get_tasks = function(req, res) {
     res.status(200).send(r);
 }
 
- 
+//function to extract independent task steps and decompose into manageable objects
 var decompose = function(response){
     var steps = [];
     for(const v of response){
@@ -92,24 +79,19 @@ var decompose = function(response){
         
         
     }
-    for(const stepsList of steps){
-       //console.log(JSON.stringify(stepsList) +'\n');
-    }    
     return steps;
     
     
 }
 
 
-//function takes filename and role = roles
+//function to get headers and steps from task files 
 var get_steps = function(filename, role) {
     //estalblish header keys:pairs here to sync with each step
     var response = [];
     const file = yaml.safeLoad(fs.readFileSync(taskDir+filename, "utf8"));
     var title = {title: file.title};
     taskKeyPair = {taskNumber: taskCounter++};
-    //response.push(taskKeyPair);
-    //response.push(title);
     for(i=0; i<file.roles.length; i++){
         var actorRole = file.roles[i];
         for(property in actorRole){
@@ -118,7 +100,7 @@ var get_steps = function(filename, role) {
             }
         }
     }
-    //console.log(title);
+  
     for(const step of file.steps)
     {
         var keys = Object.keys(step);
@@ -129,10 +111,7 @@ var get_steps = function(filename, role) {
                     continue;
                 else if(step[key].length != undefined && step[key].length === 1)
                     response.push([{MaestroStep:step[key]}]);
-                //else
-                //{
-                    //response.push(...step[key]);
-                //}
+               
                 else if(step[key].length != undefined && step[key].length > 1)
                     response.push([{MaestroStep:step[key]}]); 
             }
@@ -142,14 +121,12 @@ var get_steps = function(filename, role) {
                 else if(step[key][role].length != undefined && step[key][role].length === 1){
                         response.push([{MaestroStep:step[key][role]}]);
                 }
-                //else
-                    //response.push(...step[key][role]);
-                    else if(step[key][role].length != undefined && step[key][role].length > 1){
+                else if(step[key][role].length != undefined && step[key][role].length > 1){
                         response.push([{MaestroStep:step[key][role]}]);
                 }
             }
         }
     }
-    //console.log(response);
+   
     return response;
 }
