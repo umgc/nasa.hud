37f68650598f8207bddd06378327dcf072e8c381
Ryan Cohan <rcohan08@gmail.com>
2020-04-14 21:04:54 -0400

    Fixed taskController test and added test coverage files to gitignore

--- backend/test/tasksController.spec.js
+++ backend/test/tasksController.spec.js
@@ -3,15 +3,15 @@ const expect = require('chai').expect;
 const app = require('../app');
 
 describe('tasks controller', function () {
-  // it('should return JSON of steps', () => {
-  //     return request(app)
-  //         .get('/hud/api/tasks/procedures_EVA1.yml/EV1')
-  //         .expect('Content-Type', 'application/json; charset=utf-8')
-  //         .expect(200)
-  //         .then(async (res) => {
-  //             expect(res.body).to.be.an('array');
-  //         });
-  // });
+  it('should return JSON of steps', () => {
+      return request(app)
+          .get('/hud/api/tasks/procedures_EVA1.yml/EV1')
+          .expect('Content-Type', 'application/json; charset=utf-8')
+          .expect(200)
+          .then(async (res) => {
+              expect(res.body).to.be.an('Object');
+          });
+  });
   it('should return 404 File Not Found', () => {
     return request(app)
       .get('/hud/api/tasks/procedures_MISSING.yml/dude')
